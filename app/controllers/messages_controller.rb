a02a5437ed85dc6444beb61cf0d0376fc1f35763
kbatoev <kbatoev@mail.ru>
2016-09-03 17:51:48 +0300

    Notifications are created after sending the messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,6 +7,15 @@ class MessagesController < ApplicationController
   def create
     @task = Task.find(params[:message][:task_id])
     @message = @task.messages.create(messages_params)
+
+    teacher = @task.job.assignment.term.course.teacher
+    student = @task.student.user
+    if teacher.id == @message.user_id
+      addressee = student
+    else
+      addressee = teacher
+    end
+    Notification.new_event(user: addressee, task: @task, event_type: :added_message, event_time: @message.created_at)
   end
 
   private
